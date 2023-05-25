aa8e507ed6c0a9a8ec61dd71413d6910048ac743
Bruno Denisov <bruno.denisov9@gmail.com>
2023-05-25 13:44:31 +0200

    Get with primitive and complex types of paramater.

---
+++ WebApiPratice/WebApiPratice.WebApi/Controllers/CarController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiPratice.WebApi.Controllers
+{
+    public class Info
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+    public class CarController : ApiController
+    {
+        // GET: api/Car
+        public string Get(int id, string name)
+        {
+            return (Convert.ToString(id) + name);
+        }
+
+        // GET: api/Car
+        public string Get([FromBody] Info info)
+        {
+            return (info.Name + info.Id);
+        }
+
+        // POST: api/Car
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Car/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Car/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
