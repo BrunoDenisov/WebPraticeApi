0fe141af80f532db305af52b1e1a58c956476cb7
Bruno Denisov <bruno.denisov9@gmail.com>
2023-05-25 13:10:47 +0200

    Created Solution

---
+++ WebApiPratice/WebApiPratice.WebApi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace WebApiPratice.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
